5d5f43273859384f31d24e42974d91eb2e0419d6
zootankb <1906375838@qq.com>
2020-08-22 14:45:56 +0800

    添加客户端页面

---
+++ WebsocketServer/WebsocketClientForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WebsocketServer
+{
+    partial class WebsocketClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbHistoryTotal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbHistory = new System.Windows.Forms.TextBox();
+            this.tbSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(558, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "消息历史：";
+            // 
+            // tbHistoryTotal
+            // 
+            this.tbHistoryTotal.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tbHistoryTotal.Location = new System.Drawing.Point(560, 28);
+            this.tbHistoryTotal.Multiline = true;
+            this.tbHistoryTotal.Name = "tbHistoryTotal";
+            this.tbHistoryTotal.ReadOnly = true;
+            this.tbHistoryTotal.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbHistoryTotal.Size = new System.Drawing.Size(244, 511);
+            this.tbHistoryTotal.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(295, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "接收消息：";
+            // 
+            // tbHistory
+            // 
+            this.tbHistory.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tbHistory.Location = new System.Drawing.Point(297, 29);
+            this.tbHistory.Multiline = true;
+            this.tbHistory.Name = "tbHistory";
+            this.tbHistory.ReadOnly = true;
+            this.tbHistory.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbHistory.Size = new System.Drawing.Size(226, 511);
+            this.tbHistory.TabIndex = 6;
+            // 
+            // tbSend
+            // 
+            this.tbSend.Location = new System.Drawing.Point(14, 67);
+            this.tbSend.Multiline = true;
+            this.tbSend.Name = "tbSend";
+            this.tbSend.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbSend.Size = new System.Drawing.Size(240, 444);
+            this.tbSend.TabIndex = 4;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(179, 517);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "发送消息";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(179, 38);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "关闭链接";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(95, 38);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "开始链接";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(95, 11);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(159, 21);
+            this.tbAddress.TabIndex = 1;
+            this.tbAddress.Text = "8001";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "服务器地址：";
+            // 
+            // WebsocketClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 551);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbHistoryTotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbHistory);
+            this.Controls.Add(this.tbSend);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "WebsocketClientForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Websocket_客户端";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbHistoryTotal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbHistory;
+        private System.Windows.Forms.TextBox tbSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.Label label1;
+    }
+}
